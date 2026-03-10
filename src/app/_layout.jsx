f26817aa604b91e8050af647b4e4e3f44bd7a95b
Snooze03 <baayjohnzeus@gmail.com>
2026-03-10 22:02:54 +0800

    Style: hid the header for index and transparent header for scan

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -2,5 +2,19 @@ import '../styles/global.css';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-    return <Stack />;
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen
+                name="camera/scan"
+                options={{
+                    headerBackButtonDisplayMode: 'generic',
+                    headerTitle: 'Back',
+                    headerTransparent: true,
+                    headerBlurEffect: 'systemMaterialDark',
+                    headerTintColor: 'white',
+                }}
+            />
+        </Stack>
+    );
 }
