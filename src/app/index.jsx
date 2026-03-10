2d0e1e78a65e1ed1053f24077b5ab1f500dc1988
Uhkirruh <LCC@LTP1075TA.lccgroup.com>
2026-03-10 13:11:54 +0800

    added index

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,51 @@
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { ScanHere } from '@/components/scan';
+import { LogoLCC } from '@/components/logolcc';
+import { PriceCheckBox } from '@/components/pricecheckerbox';
 
 export default function HomeScreen() {
     return (
-        <View
-            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        <ImageBackground
+            source={require('../assets/supermarket.jpg')}
+            style={styles.background}
+            resizeMode="cover"
         >
-            <Text className="font-bold">Home Screen Works!</Text>
-        </View>
+            <View style={styles.container}>
+                <Text style={styles.bannerplaceholder}>BANNER PLACEHOLDER</Text>
+                <View style={styles.placeholderbox}>
+                    <LogoLCC />
+                    <PriceCheckBox />
+                    <ScanHere />
+                </View>
+            </View>
+        </ImageBackground>
     );
 }
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    },
+    container: {
+        flex: 1,
+    },
+    //  font-bold ">
+    placeholderbox: {
+        flex: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 2,
+        paddingTop: 56,
+    },
+    bannerplaceholder: {
+        flex: 0,
+        justifyContent: 'center',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderRadius: 6,
+        height: 250,
+        fontWeight: 'bold',
+    },
+});
