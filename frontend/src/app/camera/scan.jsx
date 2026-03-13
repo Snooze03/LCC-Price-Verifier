9aa1c12ca0c4e572b6e257a5838c5db1c27778fa
Snooze <baayjohnzeus@gmail.com>
2026-03-13 16:15:32 +0800

    Refactor: added state to update barcode data

--- frontend/src/app/camera/scan.jsx
+++ frontend/src/app/camera/scan.jsx
@@ -1,3 +1,4 @@
+import { use, useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { CameraView } from 'expo-camera';
 import { useRouter } from 'expo-router';
@@ -6,13 +7,16 @@ import { usePriceVerifier } from '@/hooks/usePriceVerifier';
 import { BARCODE_TYPES } from '@/constants/barcodeTypes';
 
 export default function ScanBarcode() {
+    const [barcode, setBarcode] = useState(null);
     const router = useRouter();
-    const { price } = usePriceVerifier();
+    const { price, isLoading, isSuccess } = usePriceVerifier(barcode);
 
-    function handleBarcodeScanned({ data }) {
-        console.log(price?.data.message);
-        router.back();
-    }
+    useEffect(() => {
+        if (isSuccess && price) {
+            console.log(price);
+            router.back();
+        }
+    }, [price, isSuccess]);
 
     return (
         <View style={styles.container}>
@@ -23,7 +27,7 @@ export default function ScanBarcode() {
                 barcodeScannerSettings={{
                     barcodeTypes: [...BARCODE_TYPES],
                 }}
-                onBarcodeScanned={handleBarcodeScanned}
+                onBarcodeScanned={({ data }) => setBarcode(data)}
             />
         </View>
     );
