5236fe600c8ca958fe4e1ffeffad2c396d2d01f5
Snooze <baayjohnzeus@gmail.com>
2026-03-13 16:14:19 +0800

    Fixed: only query if barcode is provided. Without it, it can return empty values

--- frontend/src/hooks/usePriceVerifier.js
+++ frontend/src/hooks/usePriceVerifier.js
@@ -1,15 +1,19 @@
-import { useState } from 'react';
 import { useQuery } from 'node_modules/@tanstack/react-query/build/legacy';
-
 import { api } from '@/api/backend';
 
 export function usePriceVerifier(barcode) {
     const price = useQuery({
-        queryKey: [barcode],
+        queryKey: [`code: ${barcode}`],
         queryFn: async () => {
-            return await api.get('/price');
+            return await api.get(`/price/${barcode}`);
         },
+        enabled: !!barcode && barcode !== 'null',
     });
 
-    return { price: price.data };
+    return {
+        price: price.data?.data,
+        isLoading: price.isFetching,
+        isSuccess: price.isSuccess,
+        isError: price.isError,
+    };
 }
