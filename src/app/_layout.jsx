8886609ef2eb831108c08d87002ff2922fa4d9be
Snooze <baayjohnzeus@gmail.com>
2026-03-13 10:45:47 +0800

    Added: query provider for tanstack query

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,19 +1,27 @@
 import { Stack } from 'expo-router';
+import {
+    QueryClient,
+    QueryClientProvider,
+} from 'node_modules/@tanstack/react-query/build/legacy';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen
-                name="camera/scan"
-                options={{
-                    headerBackButtonDisplayMode: 'generic',
-                    headerTitle: 'Back',
-                    headerTransparent: true,
-                    headerBlurEffect: 'systemMaterialDark',
-                    headerTintColor: 'white',
-                }}
-            />
-        </Stack>
+        <QueryClientProvider client={queryClient}>
+            <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen
+                    name="camera/scan"
+                    options={{
+                        headerBackButtonDisplayMode: 'generic',
+                        headerTitle: 'Back',
+                        headerTransparent: true,
+                        headerBlurEffect: 'systemMaterialDark',
+                        headerTintColor: 'white',
+                    }}
+                />
+            </Stack>
+        </QueryClientProvider>
     );
 }
