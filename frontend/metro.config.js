5873894435fb50f1eba735b8f5936f907b152dfd
Snooze <baayjohnzeus@gmail.com>
2026-03-19 13:50:05 +0800

    Deleted

--- frontend/metro.config.js
+++
@@ -1,5 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// module.exports = withNativeWind(config, { input: './src/styles/global.css' });
