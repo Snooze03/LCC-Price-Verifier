94aed3f90d9b0c3c665a0ba62a58154bf4b6c423
Snooze <baayjohnzeus@gmail.com>
2026-03-19 17:18:54 +0800

    Refactor: changed SQL query to query IUPC intead of SKU, and added error message for non existing products

--- backend/routes/price.js
+++ backend/routes/price.js
@@ -3,9 +3,20 @@ export default async function itemRoutes(FASTIFY, options) {
         const { barcode } = request.params;
 
         const [rows] = await FASTIFY.mysql.query(
-            'SELECT description, price FROM price_table WHERE primary_sku =?',
+            `SELECT barcode.INUMBR, barcode.IUPC, price_table.description, price_table.price
+            FROM barcode
+            INNER JOIN price_table 
+            ON barcode.INUMBR = price_table.primary_sku
+            WHERE barcode.IUPC = ?`,
             [barcode],
         );
+
+        if (!rows || rows.length === 0) {
+            return reply.code(404).send({
+                error: 'Not Found',
+                message: `No product found with barcode ${barcode}`,
+            });
+        }
 
         const item = rows[0];
 
