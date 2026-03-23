6252a60cf6a2fa1726e2a3043987f1e0ebd9b3d2
Snooze <baayjohnzeus@gmail.com>
2026-03-23 15:57:07 +0800

    New: Initial admin routes

---
+++ frontend/src/app/(modes)/admin/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Promotions() {
+    return (
+        <View>
+            <Text>Hello admin Panel</Text>
+        </View>
+    );
+}
