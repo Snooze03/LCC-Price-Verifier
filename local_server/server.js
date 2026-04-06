450106eff117d64ba5cb77d4c9179090a9f457e1
Snooze <baayjohnzeus@gmail.com>
2026-04-06 16:14:12 +0800

    Added: console login prompt

--- local_server/server.js
+++ local_server/server.js
@@ -1,18 +1,27 @@
 import fastify from 'fastify';
+import { consoleLogin } from './plugins/consoleLogin.js';
 import dbConnector from './plugins/dbConnector.js';
-import { priceRoutes } from './routes/price.js';
+import { priceRoutes } from './routes/local.js';
 
 const FASTIFY = fastify({ logger: true });
 
+// Local Server Flow
+// 1. Fetch config from CENTRAL DB
+// 2. Configure local server with fetched config
+// 3. Register routes
 const start = async () => {
+    // Authenticate local server
+    const remoteToken = await consoleLogin();
+    console.log(remoteToken);
+
     try {
         await FASTIFY.register(dbConnector);
         await FASTIFY.register(priceRoutes);
 
         await FASTIFY.listen({ port: 3000, host: '0.0.0.0' });
         console.log('Server is running on port 3000');
-    } catch (err) {
-        FASTIFY.log.error(err);
+    } catch (error) {
+        FASTIFY.log.error('Fastify Error: ', error);
         process.exit(1);
     }
 };
