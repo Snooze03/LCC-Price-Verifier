cf3a623b58db46d1964c605658fabc269edf389f
Snooze <baayjohnzeus@gmail.com>
2026-04-07 13:56:13 +0800

    Refactor: refactored base server file
    set zod for validator compiler and serializer, added graceful shutdown

--- central_server/server.js
+++ central_server/server.js
@@ -1,20 +1,42 @@
 import fastify from 'fastify';
+import {
+    serializerCompiler,
+    validatorCompiler,
+} from 'fastify-type-provider-zod';
 import dbConnector from './plugins/dbConnector.js';
 import { configRoutes } from './routes/config.js';
+import { authRoutes } from './routes/auth.js';
 
-const FASTIFY = fastify({ logger: true });
+const FASTIFY = fastify({
+    logger: {
+        transport: {
+            target: 'pino-pretty',
+        },
+    },
+});
+
+// Set fastify zod settings
+FASTIFY.setValidatorCompiler(validatorCompiler);
+FASTIFY.setSerializerCompiler(serializerCompiler);
 
 const start = async () => {
-    try {
-        await FASTIFY.register(dbConnector);
-        await FASTIFY.register(configRoutes);
+    // Register Plugins
+    await FASTIFY.register(dbConnector);
 
-        await FASTIFY.listen({ port: 3000, host: '0.0.0.0' });
-        console.log('Server is running on port 3000');
-    } catch (err) {
-        FASTIFY.log.error(err);
-        process.exit(1);
-    }
+    // Register Routes
+    await FASTIFY.register(configRoutes);
+    await FASTIFY.register(authRoutes);
+
+    await FASTIFY.listen({ port: 3001, host: '0.0.0.0' });
 };
+
+// graceful shutdown
+['SIGINT', 'SIGTERM'].forEach((signal) => {
+    process.on(signal, async () => {
+        await FASTIFY.close();
+
+        process.exit(0);
+    });
+});
 
 start();
