574294c31c0becd9b7ebb9140186ae1f026ac58a
Snooze <baayjohnzeus@gmail.com>
2026-04-08 16:23:20 +0800

    Removed try catch

--- local_server/plugins/consoleLogin.js
+++ local_server/plugins/consoleLogin.js
@@ -1,52 +1,30 @@
-import { input, password } from '@inquirer/prompts';
+import { number, password } from '@inquirer/prompts';
 import 'dotenv/config';
 
 export async function consoleLogin(FASTIFY, options) {
-    let userName = '',
-        passWord = '';
+    let store_id = null;
+    let passWord = '';
 
-    try {
-        console.log('---------- CENTRAL SERVER AUTHENTICATION ----------');
-        while (true) {
-            userName = await input({
-                message: 'Enter Store Number: ',
-                validate: async (value) => {
-                    const check = await fetch(
-                        `${process.env.CENTRAL_SERVER}/auth/${value}`,
-                    );
-                    if (value.length === 0) {
-                        return 'Please enter Store Number';
-                    } else if (check.status === 404) {
-                        return check.statusText;
-                    } else if (check.status === 200) {
-                        return check.ok;
-                    }
-                },
-            });
-            passWord = await password({
-                message: 'Enter Password: ',
-                mask: '*',
-                validate: (value) => {
-                    if (value.length === 0) {
-                        return 'Please enter Password';
-                    }
+    console.log('---------- CENTRAL SERVER AUTHENTICATION ----------');
+    while (true) {
+        store_id = await number({
+            message: 'Enter Store Number: ',
+            validate: (value) => (value ? true : 'Please enter Store Number'),
+        });
+        passWord = await password({
+            message: 'Enter Password: ',
+            mask: '*',
+            validate: (value) => (value ? true : 'Please enter Password'),
+        });
 
-                    return true;
-                },
-            });
-            console.log('---------- AUTHENTICATION SUCCESS ----------');
+        console.log('---------- AUTHENTICATION SUCCESS ----------');
 
-            // after successful login, fetch config from central server
-            fetch(`${process.env.CENTRAL_SERVER}/config/backend/${userName}`)
-                .then((response) => response.json())
-                .then((data) => console.log(data))
-                .catch((error) => console.log(error));
+        // validate login credentials
+        fetch(`${process.env.CENTRAL_SERVER}/auth/login`, { method: 'POST' })
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error));
 
-            break;
-        }
-    } catch (error) {
-        console.log('Authentication Error:', error);
+        break;
     }
-
-    return 'success!';
 }
