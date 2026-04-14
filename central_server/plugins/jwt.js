2cd471e5050d2f2336486778ef686b876a89ace3
Snooze <baayjohnzeus@gmail.com>
2026-04-14 08:45:53 +0800

    feat: added a custom jwt authenticate decorator
    used to validate jwt token from client

--- central_server/plugins/jwt.js
+++ central_server/plugins/jwt.js
@@ -7,6 +7,16 @@ async function jwtToken(FASTIFY, options) {
         secret: process.env.JWT_SECRET_KEY,
     });
 
+    FASTIFY.decorate('jwtAuthenticate', async function (request, reply) {
+        try {
+            await request.jwtVerify();
+        } catch (error) {
+            FASTIFY.log.error({ err: error }, 'JWT verification failed');
+            reply.code(401).send({ message: 'Unauthorized!' });
+            return;
+        }
+    });
+
     FASTIFY.log.info('Plugins: JWT Token Registered');
 }
 
