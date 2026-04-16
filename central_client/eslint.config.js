51328612c1115e42d17047b3a4f022f76eaaa819
Snooze <baayjohnzeus@gmail.com>
2026-04-16 10:11:56 +0800

    Chore: refactored eslint config

--- central_client/eslint.config.js
+++ central_client/eslint.config.js
@@ -1,29 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default defineConfig([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{js,jsx}"],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
-      },
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.{js,jsx}'],
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'react/no-unknown-property': ['error', { ignore: ['css'] }],
+        },
     },
-    rules: {
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-    },
-  },
-]);
+    eslintConfigPrettier,
+];
