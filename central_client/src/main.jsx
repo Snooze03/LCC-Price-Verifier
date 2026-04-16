2ad318336d9b691f1dc603bfed9eae163e195a8d
Snooze <baayjohnzeus@gmail.com>
2026-04-16 10:20:11 +0800

    Chore: massive clean up

--- central_client/src/main.jsx
+++ central_client/src/main.jsx
@@ -1,38 +1,35 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import App from "./App.jsx";
-import LoginPage from "./components/loginPage.jsx";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import AdminStorePage from "./components/storeAdminPage.jsx";
-import Testing from "./components/testing.jsx";
+import '@/styles/index.css';
+import LoginPage from '@/components/loginPage';
+import StorePage from '@/components/storeAdminPage';
+import BranchAccounts from '@/pages/branchAccounts.jsx';
+import Configuration from '@/pages/configuration.jsx';
 
-import BranchAccounts from "./pages/branchAccounts";
-import Configuration from "./pages/configuration";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* <App /> */}
-        {/* <LoginPage /> */}
-        {/* <AdminStorePage /> */}
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        {/* <Route path="/dashboard" element={<Testing />}> */}
-        <Route path="/dashboard" element={<AdminStorePage />}>
-          <Route
-            index
-            element={
-              <p className="flex justify-center">THIS IS THE MAIN CENTER</p>
-            }
-          />
-          <Route path="branch-accounts" element={<BranchAccounts />} />
-          <Route path="configuration" element={<Configuration />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/login" />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/dashboard" element={<StorePage />}>
+                    <Route
+                        index
+                        element={
+                            <p className="flex justify-center">
+                                THIS IS THE MAIN CENTER
+                            </p>
+                        }
+                    />
+                    <Route
+                        path="branch-accounts"
+                        element={<BranchAccounts />}
+                    />
+                    <Route path="configuration" element={<Configuration />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 );
