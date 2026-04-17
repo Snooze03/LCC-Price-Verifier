443a7ccd759c6b729a3ae2d8a3949f8eaa2232fc
Snooze <baayjohnzeus@gmail.com>
2026-04-17 16:55:21 +0800

    New: console login, in progress

--- local_server/plugins/consoleLogin.js
+++ local_server/plugins/consoleLogin.js
@@ -1,4 +1,5 @@
 import { number, password } from '@inquirer/prompts';
+import { api, addToken } from '../api/api.js';
 import 'dotenv/config';
 
 export async function consoleLogin(FASTIFY, options) {
@@ -6,25 +7,34 @@ export async function consoleLogin(FASTIFY, options) {
     let passWord = '';
 
     console.log('---------- CENTRAL SERVER AUTHENTICATION ----------');
-    while (true) {
-        store_id = await number({
-            message: 'Enter Store Number: ',
-            validate: (value) => (value ? true : 'Please enter Store Number'),
-        });
-        passWord = await password({
-            message: 'Enter Password: ',
-            mask: '*',
-            validate: (value) => (value ? true : 'Please enter Password'),
-        });
 
-        console.log('---------- AUTHENTICATION SUCCESS ----------');
+    store_id = await number({
+        message: 'Enter Store Number: ',
+        validate: async (value) => {
+            // Add validation: if store exists!
+            if (!value) return 'Please enter a Store Number';
 
-        // validate login credentials
-        fetch(`${process.env.CENTRAL_SERVER}/auth/login`, { method: 'POST' })
-            .then((response) => response.json())
-            .then((data) => console.log(data))
-            .catch((error) => console.log(error));
+            return true;
+        },
+    });
 
-        break;
-    }
+    passWord = await password({
+        message: 'Enter Password: ',
+        mask: '*',
+        validate: (value) => (value ? true : 'Please enter Password'),
+    });
+
+    // send request
+    const response = await api.post(`auth/login`, {
+        store_id,
+        password: passWord,
+    });
+
+    const access_token = response.data.body.access_token;
+    console.log('RESPONSE: ', response);
+
+    // add token to axios response interceptor
+    await addToken(access_token);
+
+    console.log('---------- AUTHENTICATION SUCCESS ----------');
 }
