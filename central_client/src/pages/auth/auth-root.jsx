311a1b116b8cf351b136bb5b4cda702be03e387e
Snooze03 <baayjohnzeus@gmail.com>
2026-04-19 12:15:56 +0800

    Installed shadcn login form

---
+++ central_client/src/pages/auth/auth-root.jsx
@@ -0,0 +1,24 @@
+import brandLogo from '@/assets/brandLogo.png';
+import { LoginForm } from '@/pages/auth/LoginForm';
+
+export function AuthRoot() {
+    return (
+        <>
+            {/* Header */}
+            <div className="flex flex-col justify-center items-center mb-6">
+                <img
+                    src={brandLogo}
+                    alt="Logo"
+                    className="w-30 h-30 object-contain"
+                />
+                <p className="font-bold text-xl">Liberty Commercial Center</p>
+                <p className="text-xs text-gray-400">
+                    Price Verifier Admin Portal
+                </p>
+            </div>
+
+            {/* Form */}
+            <LoginForm className="w-sm" />
+        </>
+    );
+}
