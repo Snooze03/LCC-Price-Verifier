c104268fda4a764dda6ccd15ed6d87e69194697c
Snooze03 <baayjohnzeus@gmail.com>
2026-04-19 10:01:05 +0800

    Config: added layout path alias

--- central_client/vite.config.js
+++ central_client/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
+            '@layouts': path.resolve(__dirname, './src/components/layouts'),
             '@ui': path.resolve(__dirname, './src/components/ui'),
         },
     },
