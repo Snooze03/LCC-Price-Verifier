c9b35bee1c99a0c4a0a2c230f0743a1c21669701
Snooze <baayjohnzeus@gmail.com>
2026-04-22 10:42:07 +0800

    Feat: migrated price ver config route to prisma

--- central_server/src/routes/remote/config.js
+++ central_server/src/routes/remote/config.js
@@ -1,37 +1,20 @@
 export async function configRoutes(FASTIFY, options) {
     // Route for fetching local store backend server config
-    // (db connection string, user name, etc...)
     FASTIFY.get(
         '/config',
         {
             onRequest: [FASTIFY.authenticate],
         },
         async (request, reply) => {
-            const { store_id } = request.user;
+            const storeID = request.user.store_id;
 
-            const [rows] = await FASTIFY.mysql.query(
-                `SELECT store_id, db_connection_string, db_user_name, db_password, image_path
-                FROM config
-                WHERE store_id = ?`,
-                [store_id],
-            );
+            const rawConfigData = await FASTIFY.prisma.config.findFirst({
+                where: { store_id: storeID },
+            });
 
-            if (!rows || rows.length === 0) {
-                return reply.code(404).send({
-                    error: 'Not Found',
-                    message: `No store config found with id: ${store_id}`,
-                });
-            }
+            const { id, store_id, ...configData } = rawConfigData;
 
-            const item = rows[0];
-
-            return {
-                store_id: item.store_id,
-                db_connection_string: item.db_connection_string,
-                db_user_name: item.db_user_name,
-                db_password: item.db_password,
-                image_path: item.image_path,
-            };
+            return configData;
         },
     );
 
