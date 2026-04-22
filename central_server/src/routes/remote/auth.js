e752fe71e6bac850fe11c871e822132c226ef8f4
Snooze <baayjohnzeus@gmail.com>
2026-04-22 10:41:46 +0800

    Feat: migrated remote auth login route to prisma

--- central_server/src/routes/remote/auth.js
+++ central_server/src/routes/remote/auth.js
@@ -8,44 +8,45 @@ export async function remoteAuth(FASTIFY, options) {
         },
         async (request, reply) => {
             // Request data
-            const store_id = request.body.store_id;
-            const password = request.body.password;
+            const { store_id, password } = request.body;
 
-            const [rows] = await FASTIFY.mysql.query(
-                `SELECT store_id, password
-                FROM stores
-                WHERE store_id = ?`,
-                [store_id],
-            );
+            // Check if account exists
+            const account = await FASTIFY.prisma.stores.findUnique({
+                where: { store_id },
+            });
 
-            // Checks if store exists
-            if (rows.length === 0) {
-                return reply.code(404).send({
-                    message: 'Store Not Found',
-                });
+            if (account) {
+                // Check if password is correct
+                const hashedPassword = account.password;
+                const isCorrect = await FASTIFY.verify(
+                    hashedPassword,
+                    password,
+                );
+
+                if (isCorrect) {
+                    // Generate tokens
+                    const access_token = FASTIFY.jwt.access.sign({ store_id });
+                    const refresh_token = FASTIFY.jwt.refresh.sign({
+                        store_id,
+                    });
+
+                    // generate cookie with refresh token
+                    reply.setCookie('refresh_token', refresh_token).send({
+                        message: 'SUCCESS!',
+                        body: {
+                            access_token,
+                        },
+                    });
+                } else {
+                    return reply.code(401).send({
+                        message: 'Incorrect Password',
+                    });
+                }
+            } else {
+                return account;
             }
 
             // Get password from object
-            const hashedPassword = rows[0].password;
-            const isCorrect = await FASTIFY.verify(hashedPassword, password);
-
-            if (isCorrect) {
-                // Generate tokens
-                const access_token = FASTIFY.jwt.access.sign({ store_id });
-                const refresh_token = FASTIFY.jwt.refresh.sign({ store_id });
-
-                // generate cookie with refresh token
-                reply.setCookie('refresh_token', refresh_token).send({
-                    message: 'SUCCESS!',
-                    body: {
-                        access_token,
-                    },
-                });
-            } else {
-                return reply.code(401).send({
-                    message: 'Incorrect Password',
-                });
-            }
         },
     );
 
