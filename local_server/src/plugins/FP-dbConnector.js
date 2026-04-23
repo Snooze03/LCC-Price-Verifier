85b0f16aaf593f88dd0e332e482ccc7eff6a845b
Snooze <baayjohnzeus@gmail.com>
2026-04-23 14:43:38 +0800

    Chore: used config plugin to get config

--- local_server/src/plugins/FP-dbConnector.js
+++ local_server/src/plugins/FP-dbConnector.js
@@ -1,16 +1,9 @@
-import 'dotenv/config';
-import { readFile } from 'node:fs/promises';
-import path from 'node:path';
 import fastifyPlugin from 'fastify-plugin';
 import fastifyMysql from '@fastify/mysql';
 
 async function dbConnectorFP(FASTIFY, options) {
-    const rootDir = process.cwd();
-    const configPath = path.join(rootDir, 'config.json');
-
-    const rawData = await readFile(configPath, 'utf8');
     const { connection_type, db_user, db_password, host, port, db_name } =
-        JSON.parse(rawData);
+        FASTIFY.config;
 
     const connectionString =
         connection_type +
@@ -27,7 +20,6 @@ async function dbConnectorFP(FASTIFY, options) {
 
     // console.log(connectionString);
 
-    // mysql://pricever:pricever@10.0.3.46:3306/price_ver
     FASTIFY.register(fastifyMysql, {
         promise: true,
         connectionString: connectionString.toString(),
