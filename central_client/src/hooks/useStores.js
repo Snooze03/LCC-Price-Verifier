343350fa19c828cb0ca4fd08a9a1ba0c2211027a
Snooze <baayjohnzeus@gmail.com>
2026-04-24 17:07:37 +0800

    Chore: minor refactor

--- central_client/src/hooks/useStores.js
+++ central_client/src/hooks/useStores.js
@@ -2,18 +2,20 @@ import { api } from '@/api/api';
 import { useQuery } from '@tanstack/react-query';
 
 export function useStores() {
-    const query = useQuery({
+    const getStores = useQuery({
         queryKey: ['stores'],
         queryFn: async () => {
             const response = await api.get('stores');
             return response.data;
         },
+        staleTime: 5 * 60 * 1000,
+        retry: 3,
     });
 
     return {
-        data: query.data,
-        isLoading: query.isLoading,
-        isError: query.isError,
-        error: query.error,
+        stores: getStores.data,
+        isPending: getStores.isPending,
+        isError: getStores.isError,
+        error: getStores.error,
     };
 }
