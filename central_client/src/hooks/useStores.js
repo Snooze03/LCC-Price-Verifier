c6aae2e350234f63178dbcd86df6fb3307ba67c0
Snooze03 <baayjohnzeus@gmail.com>
2026-04-25 19:04:02 +0800

    Delete and Refactor: major refactor compressing hooks and improving readability

--- central_client/src/hooks/useStores.js
+++ central_client/src/hooks/useStores.js
@@ -1,8 +1,10 @@
 import { api } from '@/api/api';
-import { useQuery } from '@tanstack/react-query';
+import { useQueryClient, useQuery, useMutation } from '@tanstack/react-query';
 
 export function useStores() {
-    const getStores = useQuery({
+    const queryClient = useQueryClient();
+
+    const get = useQuery({
         queryKey: ['stores'],
         queryFn: async () => {
             const response = await api.get('stores');
@@ -12,10 +14,47 @@ export function useStores() {
         retry: 3,
     });
 
+    const deleteRequest = useMutation({
+        mutationKey: ['store-delete'],
+        mutationFn: async (store_id) => {
+            const response = await api.delete(`stores/${store_id}`);
+            return response.data;
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({ queryKey: ['stores'] });
+        },
+    });
+
+    const create = useMutation({
+        mutationKey: ['store-create'],
+        mutationFn: async (formData) => {
+            const response = await api.post('stores', formData);
+            return response.data;
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({ queryKey: ['stores'] });
+        },
+        onError: (error) => {
+            return error;
+        },
+        throwOnError: false,
+    });
+
     return {
-        stores: getStores.data,
-        isPending: getStores.isPending,
-        isError: getStores.isError,
-        error: getStores.error,
+        // Get data and states
+        stores: get.data,
+        isPending: get.isPending,
+        isError: get.isError,
+        error: get.error,
+
+        // Delete data and states
+        deleteStore: deleteRequest.mutate,
+        isDeleting: deleteRequest.isPending,
+        response: deleteRequest.data,
+
+        createStore: create.mutateAsync,
+        isCreating: create.isPending,
+        isCreateError: create.isError,
+        createError: create.error,
     };
 }
