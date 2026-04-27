a537cf4e23f646c088bb70653b4b0d83a78d080e
Snooze <baayjohnzeus@gmail.com>
2026-04-27 15:03:17 +0800

    Refactor: made it handle create and update operations

--- central_client/src/pages/dashboard/dialogs/account-dialog.jsx
+++ central_client/src/pages/dashboard/dialogs/account-dialog.jsx
@@ -3,7 +3,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 
 import { useAccounts } from '@/hooks/useAccounts';
 import { createAccountSchema } from '@/schemas/auth/account.schema';
-import { ACCOUNT_FIELDS } from '../add-store-fields';
+import { ACCOUNT_FIELDS } from '../form-fields';
 import {
     Dialog,
     DialogContent,
@@ -22,30 +22,74 @@ import { toast } from 'sonner';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
-export function AddAccountDialog({ onClose }) {
+export function AccountDialog({ account, onClose }) {
+    // Custom Hooks
     const { createAccount, isCreating, isCreateError, createError } =
         useAccounts();
+    const { updateAccount, isUpdating, isUpdateError, updateError } =
+        useAccounts();
+
+    let defaultValues = null;
+    const dialogState = account ? 'edit' : 'add';
+
+    // Sets default form values depending on dialog statae
+    switch (dialogState) {
+        case 'add':
+            defaultValues = {
+                email: '',
+                password: '',
+                role: '',
+            };
+            break;
+
+        case 'edit':
+            defaultValues = {
+                email: account.email,
+                password: account.password,
+                role: account.role,
+            };
+            break;
+    }
 
     const { handleSubmit, control } = useForm({
         resolver: zodResolver(createAccountSchema),
         defaultValues: {
-            email: '',
-            password: '',
-            role: '',
+            ...defaultValues,
         },
     });
 
     const onSubmit = async (data) => {
-        if (isCreateError) {
-            toast.error(`Account not created: ${createError}`, {
-                position: 'top-center',
-            });
-        } else {
-            await createAccount(data);
-            toast.success('Account Created Successfully!', {
-                position: 'top-center',
-            });
+        switch (dialogState) {
+            case 'add':
+                createAccount(data);
+
+                break;
+            case 'edit':
+                const formattedData = {
+                    id: account.id,
+                    ...data,
+                };
+                updateAccount(formattedData);
+
+                break;
         }
+
+        if (isCreateError || isUpdateError) {
+            toast.error(
+                `An Error has Occurred: ${createError || updateError}`,
+                {
+                    position: 'top-center',
+                },
+            );
+        } else {
+            toast.success(
+                `Account ${dialogState === 'add' ? 'Created' : 'Edited'}  Successfully!`,
+                {
+                    position: 'top-center',
+                },
+            );
+        }
+
         onClose();
     };
 
@@ -53,13 +97,17 @@ export function AddAccountDialog({ onClose }) {
         <Dialog open onOpenChange={onClose}>
             <DialogContent className="md:max-w-sm">
                 <DialogHeader>
-                    <DialogTitle>Add Account</DialogTitle>
+                    <DialogTitle>
+                        {dialogState === 'add' ? 'Add Account' : 'Edit Account'}
+                    </DialogTitle>
                     <DialogDescription>
-                        Fill in the following form to create a new store
+                        {dialogState === 'add'
+                            ? 'Fill in the form to create a new account'
+                            : 'Edit the values and press Update to save changes'}
                     </DialogDescription>
                 </DialogHeader>
 
-                <form id="add-store-form" onSubmit={handleSubmit(onSubmit)}>
+                <form id="store-form" onSubmit={handleSubmit(onSubmit)}>
                     <FieldSet>
                         {/* ===== Account Form ===== */}
                         <FieldGroup className="flex-col">
@@ -103,7 +151,13 @@ export function AddAccountDialog({ onClose }) {
                     {/* Form Actions */}
                     <div className="mt-6">
                         <Button className="w-full" disabled={isCreating}>
-                            {isCreating ? 'Adding Account...' : 'Add Account'}
+                            {isCreating
+                                ? 'Adding Account...'
+                                : isUpdating
+                                  ? 'Updating Account...'
+                                  : account
+                                    ? 'Update Account'
+                                    : 'Create Account'}
                         </Button>
                     </div>
                 </form>
