eab0fcdcdea4977133e385da12cd129f8fdd80db
Uhkirruh <LCC@LTP1075TA.lccgroup.com>
2026-04-27 10:53:39 +0800

    backend.js

--- local_client/src/api/backend.js
+++ local_client/src/api/backend.js
@@ -3,9 +3,14 @@ import { Platform } from 'react-native';
 
 const androidUrl = process.env.EXPO_PUBLIC_ANDROID_URL;
 const webUrl = process.env.EXPO_PUBLIC_WEB_URL;
-const baseUrl = Platform.OS === 'android' ? androidUrl : webUrl;
+// const baseUrl = Platform.OS === 'android' ? androidUrl : webUrl;
+let baseUrl = '';
 
 export const api = axios.create({
-    baseURL: baseUrl,
     timeout: 5000,
 });
+
+export function setBaseUrl(url) {
+    api.defaults.baseURL = url;
+    return baseUrl;
+}
