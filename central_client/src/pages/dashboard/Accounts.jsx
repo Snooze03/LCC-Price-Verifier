414cd9d1693a81730cc50f26039daa0e8be61e66
Snooze <baayjohnzeus@gmail.com>
2026-04-27 10:47:26 +0800

    Feat: finished account displays and creation logic

--- central_client/src/pages/dashboard/Accounts.jsx
+++ central_client/src/pages/dashboard/Accounts.jsx
@@ -1,6 +1,8 @@
 import { Plus } from 'lucide-react';
 import { useState } from 'react';
 
+import { useAccounts } from '@/hooks/useAccounts';
+import { ACCOUNT_COLUMNS } from './columns';
 import {
     Table,
     TableBody,
@@ -11,59 +13,100 @@ import {
 } from '@/components/ui/table';
 import { Button } from '@/components/ui/button';
 import { TabHeader, TabTitle } from './components/tab-header';
-import { useAccounts } from '@/hooks/useAccounts';
-import { ACCOUNT_COLUMNS } from './columns';
 import { TableActionMenu } from './components/table-action-menu';
+import { EmptyState } from '@/components/empty-state';
+import { AddAccountDialog } from './dialogs/account-dialog';
 
 function AccountsTab() {
     const { accounts, isPending, isError, error } = useAccounts();
-    console.log(accounts);
+    const [activeDialog, setActiveDialog] = useState();
 
     if (isPending) return <h1>Loading...</h1>;
 
-    const handleEditAccount = () => {};
+    const handleAddAccount = () => {
+        setActiveDialog('add');
+    };
 
-    const handleDeleteAccount = () => {};
+    const handleEditAccount = () => {
+        setActiveDialog('edit');
+    };
+
+    const handleDeleteAccount = () => {
+        setActiveDialog('delete');
+    };
+
+    const handleCloseDialog = () => {
+        setActiveDialog(null);
+    };
 
     return (
-        <div className="space-y-6">
-            <TabHeader>
-                <TabTitle>Accounts</TabTitle>
-            </TabHeader>
+        <>
+            <div className="space-y-6">
+                <TabHeader>
+                    <TabTitle>Accounts</TabTitle>
+                    <Button
+                        onClick={handleAddAccount}
+                        size="sm"
+                        className="bg-[#293041] hover:bg-[#3F4759]"
+                    >
+                        <Plus />
+                        Add Account
+                    </Button>
+                </TabHeader>
 
-            <Table className="shadow-xl">
-                <TableHeader>
-                    <TableRow className="hover:bg-inherit">
-                        {ACCOUNT_COLUMNS.map((col) => (
-                            <TableHead key={col}>{col}</TableHead>
-                        ))}
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {accounts.map((account) => (
-                        <TableRow
-                            key={account.id}
-                            className="cursor-pointer hover:bg-gray-100"
-                        >
-                            {/* Store Values */}
-                            <TableCell>{account.email}</TableCell>
-                            <TableCell className="max-w-25 truncate">
-                                {account.password}
-                            </TableCell>
-                            <TableCell>{account.role}</TableCell>
+                {accounts.length === 0 ? (
+                    <EmptyState
+                        title="No Accounts added yet"
+                        description="Add an account using the add account button"
+                    />
+                ) : (
+                    <Table className="shadow-xl">
+                        <TableHeader>
+                            <TableRow className="hover:bg-inherit">
+                                {ACCOUNT_COLUMNS.map((col) => (
+                                    <TableHead key={col}>{col}</TableHead>
+                                ))}
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {accounts.map((account) => {
+                                const [id, ...accountValues] =
+                                    Object.values(account);
 
-                            {/* Action Menu */}
-                            <TableCell>
-                                <TableActionMenu
-                                    handleEdit={handleEditAccount}
-                                    handleDelete={handleDeleteAccount}
-                                />
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </div>
+                                return (
+                                    <TableRow
+                                        key={account.id}
+                                        className="cursor-pointer hover:bg-gray-100"
+                                    >
+                                        {accountValues.map((value) => (
+                                            <TableCell
+                                                key={value}
+                                                className="max-w-25 truncate"
+                                            >
+                                                {value}
+                                            </TableCell>
+                                        ))}
+
+                                        <TableCell>
+                                            <TableActionMenu
+                                                handleEdit={handleEditAccount}
+                                                handleDelete={
+                                                    handleDeleteAccount
+                                                }
+                                            />
+                                        </TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                )}
+            </div>
+
+            {activeDialog === 'add' && (
+                <AddAccountDialog onClose={handleCloseDialog} />
+            )}
+        </>
     );
 }
 
