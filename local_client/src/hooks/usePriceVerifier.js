3f63c396ae41f221784aba6b2e93a34e7228a417
Snooze <baayjohnzeus@gmail.com>
2026-05-04 10:39:35 +0800

    Minor Change: added isPending state and staletime for cache

--- local_client/src/hooks/usePriceVerifier.js
+++ local_client/src/hooks/usePriceVerifier.js
@@ -5,15 +5,18 @@ export function usePriceVerifier(barcode) {
     const query = useQuery({
         queryKey: [`code: ${barcode}`],
         queryFn: async () => {
-            return await localAPI.get(`/price/${barcode}`);
+            const response = await localAPI.get(`/price/${barcode}`);
+            return response.data;
         },
         enabled: !!barcode && barcode !== 'null',
         retry: false,
+        staleTime: 60 * 1000,
     });
 
     return {
-        product: query.data?.data,
-        isLoading: query.isPending,
+        product: query.data,
+        isPending: query.isPending,
+        isFetching: query.isFetching,
         isSuccess: query.isSuccess,
         isError: query.isError,
     };
