2c925efe88c68acc0451a5418de7e3424a20f2cf
Snooze <baayjohnzeus@gmail.com>
2026-05-06 16:07:30 +0800

    Feat: revamped menu for local server

---
+++ local_server/src/cli/menu.js
@@ -0,0 +1,58 @@
+import { rawlist } from '@inquirer/prompts';
+import chalk from 'chalk';
+import boxen from 'boxen';
+
+import { Login } from './login.js';
+
+// Event listener for prompt cancel
+process.on('unhandledRejection', (error) => {
+    if (error instanceof Error && error.name === 'ExitPromptError') {
+        console.log(chalk.yellow('\n Cancelled Operation'));
+        process.exit(0);
+    }
+});
+
+Menu();
+
+export async function Menu() {
+    MenuHeader('Main Menu', 'Local Server');
+
+    // Menu Options
+    const selectedOption = await rawlist({
+        message: 'Select an Option',
+        choices: [
+            { name: 'Login', value: 'login' },
+            { name: 'Continue', value: 'continue' },
+            { name: 'Exit', value: 'exit' },
+        ],
+        default: 'login',
+        loop: true,
+    });
+
+    switch (selectedOption) {
+        case 'login':
+            Login();
+            break;
+        case 'continue':
+            console.log('Continue kana bro');
+            break;
+        case 'exit':
+            console.log('Exit kana bro');
+            process.exit(0);
+    }
+}
+
+export function MenuHeader(title, subTitle) {
+    console.log(
+        boxen(chalk.bold.cyan(title), {
+            title: subTitle,
+            textAlignment: 'center',
+            padding: {
+                top: 0.8,
+                right: 8,
+                left: 8,
+                bottom: 0.8,
+            },
+        }),
+    );
+}
