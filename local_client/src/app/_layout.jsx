29814779f5b6a21a2d4a4993077abc19a02167c5
Snooze <baayjohnzeus@gmail.com>
2026-05-15 17:05:12 +0800

    Attempting to fix redirect if config already exist

--- local_client/src/app/_layout.jsx
+++ local_client/src/app/_layout.jsx
@@ -1,57 +1,54 @@
-import { useEffect } from 'react';
-import { Stack, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import * as NavigationBar from 'expo-navigation-bar';
 import * as SplashScreen from 'expo-splash-screen';
-import { File, Paths } from 'expo-file-system';
+import { Directory, File, Paths } from 'expo-file-system';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { setBaseUrl } from '@/api/local.api';
 
-// Prevent splash screen hiding until we checked config file
 SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            networkMode: 'online',
-        },
-    },
+    defaultOptions: { queries: { networkMode: 'online' } },
 });
 
 export default function RootLayout() {
-    NavigationBar.setVisibilityAsync('hidden');
-    const router = useRouter();
+    const [hasConfig, setHasConfig] = useState(null);
 
-    // effect to check if config exists
     useEffect(() => {
-        async function initializeApp() {
-            try {
-                const config = new File(Paths.document, 'config', 'config.txt');
+        NavigationBar.setVisibilityAsync('hidden');
 
-                if (config.exists && config.textSync().length > 0) {
-                    await setBaseUrl(config.textSync());
-                    // comment router below when developing
-                    router.replace('store');
-                }
-            } catch (error) {
-                console.error('Check config Error: ', error);
-            } finally {
-                await SplashScreen.hideAsync();
+        try {
+            const configDirectory = new Directory(Paths.document, 'config');
+            const config = new File(configDirectory, 'config.txt');
+
+            if (configDirectory.exists && config.exists) {
+                setBaseUrl(config.textSync());
+                setHasConfig(true);
+            } else {
+                setHasConfig(false);
             }
+        } catch (error) {
+            console.error('Config evaluation failed:', error);
+            setHasConfig(false);
+        } finally {
+            SplashScreen.hideAsync();
         }
+    }, []);
 
-        initializeApp();
-    });
+    if (hasConfig === null) return null;
 
     return (
         <QueryClientProvider client={queryClient}>
             <StatusBar hidden={true} />
-            <Stack
-                screenOptions={{
-                    headerShown: false,
-                }}
-            ></Stack>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen
+                    name="index"
+                    redirect={hasConfig ? true : false}
+                />
+            </Stack>
         </QueryClientProvider>
     );
 }
