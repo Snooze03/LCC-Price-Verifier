73fe38ba4375e80041574176c62cd4aaac11aaf6
Snooze <baayjohnzeus@gmail.com>
2026-05-15 15:31:16 +0800

    Feat: auto launch price verifier if config exist

--- local_client/src/app/_layout.jsx
+++ local_client/src/app/_layout.jsx
@@ -1,7 +1,15 @@
-import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { Stack, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import * as NavigationBar from 'expo-navigation-bar';
+import * as SplashScreen from 'expo-splash-screen';
+import { File, Paths } from 'expo-file-system';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { setBaseUrl } from '@/api/local.api';
+
+// Prevent splash screen hiding until we checked config file
+SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -13,6 +21,28 @@ const queryClient = new QueryClient({
 
 export default function RootLayout() {
     NavigationBar.setVisibilityAsync('hidden');
+    const router = useRouter();
+
+    // effect to check if config exists
+    useEffect(() => {
+        async function initializeApp() {
+            try {
+                const config = new File(Paths.document, 'config', 'config.txt');
+
+                if (config.exists && config.textSync().length > 0) {
+                    await setBaseUrl(config.textSync());
+                    // comment router below when developing
+                    router.replace('store');
+                }
+            } catch (error) {
+                console.error('Check config Error: ', error);
+            } finally {
+                await SplashScreen.hideAsync();
+            }
+        }
+
+        initializeApp();
+    });
 
     return (
         <QueryClientProvider client={queryClient}>
